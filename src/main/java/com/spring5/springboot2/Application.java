76d9fe2ac944a8f7418879cfc0ae96eeb0ffe617
Lukman Arogundade <jedaademola@users.noreply.github.com>
2018-10-07 22:15:43 -0400

    Scheduling Tasks

--- src/main/java/com/spring5/springboot2/Application.java
+++ src/main/java/com/spring5/springboot2/Application.java
@@ -6,8 +6,10 @@ package com.spring5.springboot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
